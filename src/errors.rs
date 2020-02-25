4ae6774e92ecca7369b4759794a48f8b149d8832
Stephen OBrien <wayofthepie@users.noreply.github.com>
2020-02-25 22:15:00 +0000

    Add work on instruction tag parsing
    Improve the errors that we return.

--- src/errors.rs
+++ src/errors.rs
@@ -1,5 +1,6 @@
 use nom::error;
 use nom::error::{ParseError, VerboseError, VerboseErrorKind};
+use nom_locate::LocatedSpan;
 use std::borrow::Cow;
 use std::fmt::{Display, Formatter};
 
@@ -34,25 +35,29 @@ impl<'a> DockerParseError<'a> {
     }
 }
 
-impl<'a> ParseError<&'a str> for DockerParseError<'a> {
-    fn from_error_kind(input: &'a str, kind: error::ErrorKind) -> Self {
+impl<'a> ParseError<LocatedSpan<&'a str>> for DockerParseError<'a> {
+    fn from_error_kind(input: LocatedSpan<&'a str>, kind: error::ErrorKind) -> Self {
         Self {
-            nom_error: VerboseError::from_error_kind(input, kind),
+            nom_error: VerboseError::from_error_kind(input.fragment(), kind),
             message: None,
         }
     }
 
-    fn append(input: &'a str, kind: error::ErrorKind, mut other: Self) -> Self {
+    fn append(input: LocatedSpan<&'a str>, kind: error::ErrorKind, mut other: Self) -> Self {
         other
             .nom_error
             .errors
-            .push((input, VerboseErrorKind::Nom(kind)));
+            .push((input.fragment(), VerboseErrorKind::Nom(kind)));
         other
     }
-}
 
-impl<'a> Into<VerboseError<&'a str>> for DockerParseError<'a> {
-    fn into(self) -> VerboseError<&'a str> {
-        self.nom_error
+    fn add_context(input: LocatedSpan<&'a str>, ctx: &'static str, _other: Self) -> Self {
+        let context_and_location = format!(
+            "Error occurred on line {} column {}: {}",
+            input.location_line(),
+            input.get_column(),
+            ctx
+        );
+        Self::new(input.fragment(), context_and_location)
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,97 +1,162 @@
 mod errors;
+use errors::*;
 use nom::branch::alt;
 use nom::bytes::complete::tag;
-use nom::bytes::complete::take_while1;
-use nom::character::complete::{line_ending, multispace0, space0, space1};
-use nom::multi::many0;
+use nom::character::complete::{space0, space1};
+use nom::error::context;
 use nom::sequence::{preceded, terminated};
 use nom::IResult;
+use nom_locate::LocatedSpan;
 
-use errors::*;
+static NO_SPACE_AFTER_INSTRUCTION_ERROR: &str = "No whitespace found after instruction!";
 
-pub fn instruction_name<'a>(ins: &'a str) -> impl Fn(&'a str) -> IResult<&str, &str> {
-    move |input| preceded(space0, terminated(tag(ins), space1))(input)
+type Span<'a> = LocatedSpan<&'a str>;
+
+/// This macro allows us to generate all the possible supported instruction
+/// types and a useful error message in case we encounter an unknown type when
+/// parsing.
+macro_rules! instructions_enum {
+    ( $first:ident, $( $name:ident ),* ) => {
+       #[non_exhaustive]
+        pub enum Instruction {
+            $first,
+            $( $name, )*
+        }
+
+        impl Instruction {
+            pub fn from_str(string: &str) -> Option<Self> {
+                match string {
+                    stringify!($first) => Some(Self::$first),
+                    $( stringify!($name) => Some(Self::$name) )*,
+                    _ => None,
+                }
+            }
+
+            pub fn as_str(&self) -> &'static str {
+                match self {
+                    Instruction::$first => stringify!($first),
+                    $( Instruction::$name => stringify!($name) )*,
+                }
+            }
+        }
+
+        static UNSUPPORTED_INSTRUCTION_ERROR: &str =
+            stringify!(Unsupported instruction encountered, expected one of [$first $(, $name )*]);
+    }
 }
 
-pub fn instruction_argument(input: &str) -> IResult<&str, String, DockerParseError> {
-    let handle_backslash = preceded(backslash, not_backslash_or_newline);
-    let handle_full_arg = many0(alt((handle_backslash, not_backslash_or_newline)));
-    let result = terminated(handle_full_arg, multispace0)(input).unwrap();
-    if !result.0.is_empty() {
-        let error = DockerParseError::new(
-            input,
-            format!("The following tokens were not consumed: {}", result.0),
-        );
-        return Err(nom::Err::Error(error));
-    };
-    let arg = result.1.join(" ").trim().to_owned();
-    Ok((result.0, arg))
+instructions_enum!(FROM, RUN);
+
+pub fn instruction_name<'a>(span: Span) -> IResult<Span, &str, DockerParseError> {
+    let proceeding_space = context(NO_SPACE_AFTER_INSTRUCTION_ERROR, space1);
+    terminated(preceded(space0, known_instructions()), proceeding_space)(span)
 }
 
-fn not_backslash_or_newline(input: &str) -> IResult<&str, &str> {
-    let chars = |c| !"\\\n\r".contains(c);
-    take_while1(chars)(input)
-}
-
-fn backslash(input: &str) -> IResult<&str, &str> {
-    preceded(
-        space0,
-        terminated(terminated(tag("\\"), space0), line_ending),
-    )(input)
+fn known_instructions() -> impl Fn(Span) -> IResult<Span, &str, DockerParseError> {
+    fn parser(span: Span) -> IResult<Span, &str, DockerParseError> {
+        let (next_span, token) = context(
+            &UNSUPPORTED_INSTRUCTION_ERROR,
+            alt((
+                tag(Instruction::FROM.as_str()),
+                tag(Instruction::RUN.as_str()),
+            )),
+        )(span)?;
+        Ok((next_span, token.fragment()))
+    }
+    move |span| parser(span)
 }
 
 #[cfg(test)]
 mod tests {
-    use super::{backslash, instruction_argument, instruction_name};
+    use super::{instruction_name, Instruction};
+    use nom::Err as NomErr;
+    use nom_locate::LocatedSpan;
+
+    type Span<'a> = LocatedSpan<&'a str>;
 
     #[test]
-    fn should_parse_backslash_consuming_whitespace_and_single_newline() {
-        let string = "\\\n";
-        let result = backslash(string);
-        assert_eq!(result, Ok(("", "\\")));
+    fn should_fail_to_parse_known_instruction_if_not_proceeded_by_whitespace() {
+        // arrange
+        let string = format!("{}", Instruction::FROM.as_str());
+        let span = Span::new(&string);
+
+        // act
+        if let NomErr::Error(error) = instruction_name(span).err().unwrap() {
+            // assert
+            assert_eq!(
+                error.message.unwrap(),
+                "Error occurred on line 1 column 5: No whitespace found after instruction!",
+            );
+        } else {
+            panic!("Expected a specific error, did not receive it!");
+        }
     }
 
     #[test]
-    fn should_parse_instruction_name_consuming_whitespace() {
-        let string = " FROM ";
-        let result = instruction_name("FROM")(string);
-        assert_eq!(result, Ok(("", "FROM")));
+    fn should_parse_known_instruction_correctly_and_consume_proceeding_space() {
+        // arrange
+        let string = format!("   {} ", Instruction::FROM.as_str());
+        let span = Span::new(&string);
+
+        // act
+        let result = instruction_name(span);
+
+        // assert
+        assert!(result.is_ok());
+        let x = result.unwrap();
+        assert_eq!(x.1, Instruction::FROM.as_str());
+        assert_eq!(
+            x.0.fragment().to_owned(),
+            "",
+            "Expected no whitespace in remaining fragment, but received whitespace!"
+        );
     }
 
     #[test]
-    fn should_fail_parsing_instruction_name_if_name_does_not_match() {
-        let string = " RUN ";
-        let result = instruction_name("FROM")(string);
-        assert!(result.is_err());
+    fn should_parse_known_instruction_correctly_if_preceeded_by_space() {
+        // arrange
+        let string = format!("   {} ", Instruction::FROM.as_str());
+        let span = Span::new(&string);
+
+        // act
+        let result = instruction_name(span);
+
+        // assert
+        assert!(result.is_ok());
+        assert_eq!(result.unwrap().1, Instruction::FROM.as_str());
     }
 
     #[test]
-    fn should_parse_instruction_argument_consuming_proceeding_whitespace() {
-        let string = "./run.sh test \n";
-        let result = instruction_argument(string);
-        assert_eq!(result, Ok(("", "./run.sh test".to_owned())));
+    fn should_parse_known_instruction_correctly() {
+        // arrange
+        let string = format!("{} ", Instruction::FROM.as_str());
+        let span = Span::new(&string);
+
+        // act
+        let result = instruction_name(span);
+
+        // assert
+        assert!(result.is_ok());
+        assert_eq!(result.unwrap().1, Instruction::FROM.as_str());
     }
 
     #[test]
-    fn should_parse_instruction_argument_with_no_newline_consuming_proceeding_whitespace() {
-        let string = "./run.sh test";
-        let result = instruction_argument(string);
-        println!("{:#?}", result);
-        assert_eq!(result, Ok(("", "./run.sh test".to_owned())));
-    }
+    fn should_error_if_instruction_is_unknown() {
+        // arrange
+        let string = Span::new("NOT ");
 
-    #[test]
-    fn should_parse_instruction_argument_with_multiple_newlines() {
-        let string = "./run.sh test\\\ntest2\\\n  test3  ";
-        let result = instruction_argument(string);
-        println!("{:#?}", result);
-        assert_eq!(result, Ok(("", "./run.sh test test2   test3".to_owned())));
-    }
-
-    #[test]
-    fn should_fail_to_parse_instruction_argument_if_backslash_has_no_newline() {
-        let string = r#"./run.sh test\"#;
-        let result = instruction_argument(string);
-        assert!(result.is_err());
+        // act
+        if let NomErr::Error(result) = instruction_name(string).err().unwrap() {
+            // assert
+            let expected = format!(
+                "{} {} {}",
+                "Error occurred on line 1 column 1:",
+                "Unsupported instruction encountered,",
+                "expected one of [FROM, RUN]"
+            );
+            assert_eq!(result.message.unwrap(), expected);
+        } else {
+            panic!("Expected a nom error with a specific message!");
+        }
     }
 }
