1c16b2b9cf4d647ac8f5c54784aeda62f7841837
Stephen OBrien <wayofthepie@users.noreply.github.com>
2020-02-26 12:52:38 +0000

    Add doc examples, parse all preceeding whitespace

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ mod errors;
 use errors::*;
 use nom::branch::alt;
 use nom::bytes::complete::tag;
-use nom::character::complete::{space0, space1};
+use nom::character::complete::{multispace0, space1};
 use nom::error::context;
 use nom::sequence::{preceded, terminated};
 use nom::IResult;
@@ -15,7 +15,7 @@ type Span<'a> = LocatedSpan<&'a str>;
 /// This macro allows us to generate all the possible supported instruction
 /// types and a useful error message in case we encounter an unknown type when
 /// parsing.
-macro_rules! instructions_enum {
+macro_rules! generate_instructions {
     ( $first:ident, $( $name:ident ),* ) => {
        #[non_exhaustive]
         pub enum Instruction {
@@ -51,20 +51,52 @@ macro_rules! instructions_enum {
     }
 }
 
-instructions_enum!(FROM, RUN);
+generate_instructions!(FROM, RUN);
 
+/// Parse an instruction name.
+///
+/// ```
+/// use nom_locate::LocatedSpan;
+/// use dockerfile_parser::instruction_name;
+///
+/// type Span<'a> = LocatedSpan<&'a str>;
+///
+/// let dockerfile = "FROM alpine";
+///
+/// let (rest, token): (Span, &str) = instruction_name(Span::new(dockerfile)).unwrap();
+///
+/// assert_eq!(token, "FROM");
+/// assert_eq!(rest.fragment().to_owned(), "alpine");
+/// ```
 pub fn instruction_name(span: Span) -> IResult<Span, &str, DockerParseError> {
     let proceeding_space = context(NO_SPACE_AFTER_INSTRUCTION_ERROR, space1);
-    terminated(preceded(space0, known_instructions()), proceeding_space)(span)
+    terminated(
+        preceded(multispace0, known_instructions()),
+        proceeding_space,
+    )(span)
 }
 
 #[cfg(test)]
-mod tests {
+mod instruction_name_tests {
     use super::{instruction_name, Instruction};
     use nom::Err as NomErr;
     use nom_locate::LocatedSpan;
 
     type Span<'a> = LocatedSpan<&'a str>;
+
+    #[test]
+    fn should_parse_known_instruction_correctly_if_preceeded_by_any_type_of_space() {
+        // arrange
+        let string = format!("\n\r\n\t {} ", Instruction::FROM.as_str());
+        let span = Span::new(&string);
+
+        // act
+        let result = instruction_name(span);
+
+        // assert
+        assert!(result.is_ok());
+        assert_eq!(result.unwrap().1, Instruction::FROM.as_str());
+    }
 
     #[test]
     fn should_fail_to_parse_known_instruction_if_not_proceeded_by_whitespace() {
@@ -102,20 +134,6 @@ mod tests {
             "",
             "Expected no whitespace in remaining fragment, but received whitespace!"
         );
-    }
-
-    #[test]
-    fn should_parse_known_instruction_correctly_if_preceeded_by_space() {
-        // arrange
-        let string = format!("   {} ", Instruction::FROM.as_str());
-        let span = Span::new(&string);
-
-        // act
-        let result = instruction_name(span);
-
-        // assert
-        assert!(result.is_ok());
-        assert_eq!(result.unwrap().1, Instruction::FROM.as_str());
     }
 
     #[test]
